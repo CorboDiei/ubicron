a4f7b75399481cd91ffdd5f8c8978ad50e2309d1
David Corbo <corbodiei@gmail.com>
2021-05-24 11:31:01 -0700

    Adding test files

---
+++ src/parsetab.cc
@@ -0,0 +1,7 @@
+// Copyright 2021 David Corbo
+
+#include <iostream>
+#include <cstdint>
+
+#include "./parsetab.h"
+
---
+++ src/parsetab.h
@@ -0,0 +1,20 @@
+// Copyright 2021 David Corbo
+
+#ifndef PARSETAB_H_
+#define PARSETAB_H_
+
+#include <iostream>
+#include <cstdint>
+#include <string>
+
+#include "./jobs.h"
+
+using std::string;
+
+// Parses the table at the passed address, 
+// returns a new jobs object that 
+
+static jobs parseTab(string const path, );
+
+
+#endif  // PARSETAB_H_

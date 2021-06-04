d3061d06253a97a1721c32da5b98a6d450b50b0e
CorboDiei <corbodiei@gmail.com>
2021-06-03 21:10:25 -0700

    Worked on job

--- src/Jobs.h
+++ src/Jobs.h
@@ -11,27 +11,34 @@
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
 
 using namespace std;
 using namespace boost::uuids;
+namespace pt = boost::property_tree;
 
 enum FormatType { input, output, sub_triggers };
 
-bool VerifyJSON(JSONTree tree, FormatType);
-
-class JSONTree {
+class FormatTree {
     public:
-        explicit JSONTree(string )
+        explicit FormatTree(string json_text);
+
+        bool verify_tree(FormatType type);
+
+        string get_value(string query);
 
     private:
+        pt::ptree root;
+        FormatType type_;
 };
 
 class Job {
     public:
         Job(void) { }
         explicit Job(uuid id) : job_id_(id) { }
-        explicit Job(uuid id, vector<string> commands, )
-
+        explicit Job(uuid id, vector<string> commands,
+            FormatTree input, FormatTree output, FormatTree sub_trigs);
 
         uuid get_id(void) {return job_id_;}
     private:
